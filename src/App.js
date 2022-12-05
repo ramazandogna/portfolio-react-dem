f3effe1ea23191311379f10878ea8e10daff2bc6
Ramazan Doğan <doganrmzn40@gmail.com>
2022-12-06 01:20:41 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Presentation from './components/Presentation';
 
 function App() {
    return (
       <div className="App">
          <Router>
             <Navbar />
-            <Routes>{/* <Route path="/" element={<} /> */}</Routes>
+            <Routes>
+               <Route path="/" element={<Presentation />} />
+            </Routes>
          </Router>
       </div>
    );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,20 @@ import React from 'react';
 
 function Navbar() {
    return (
-      <nav className="bg-white border-b-violet-200 border-2 border-gray-400 px-2 sm:px-4 py-2.5 rounded">
+      <nav className="bg-white md:justify-center active:border-b-violet-300  border-b-violet-200 border-2 border-violet-400 px-2 sm:px-4 py-2.5 rounded">
          <div className="container flex flex-wrap items-center justify-between mx-auto">
             <Link to="/" className="flex items-center">
-               <span className="self-center text-violet-400 hover:text-violet-800 text-xl font-semibold whitespace-nowrap ">
+               <span className="self-center active:underline  text-violet-500  hover:text-violet-800 text-xl font-semibold whitespace-nowrap ">
                   R.D Portfolio
                </span>
             </Link>
 
-            <div
-               className="hidden w-full md:block md:w-auto"
-               id="navbar-default"
-            >
+            <div className=" w-full md:block md:w-auto" id="navbar-default">
                <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-violet-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
                   <li>
                      <Link
                         to="/"
-                        className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
+                        className="block py-2 pl-3 pr-4 active:underline  text-white bg-violet-700 rounded md:bg-transparent md:text-violet-700 md:p-0"
                         aria-current="page"
                      >
                         Home
@@ -28,7 +25,7 @@ function Navbar() {
                   <li>
                      <Link
                         to="/about"
-                        className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 "
+                        className="block py-2 pl-3 pr-4 active:underline  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 "
                      >
                         About
                      </Link>
@@ -36,7 +33,7 @@ function Navbar() {
                   <li>
                      <Link
                         to="/about"
-                        className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 "
+                        className="block py-2 pl-3 pr-4 active:underline  text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-violet-800 md:p-0 "
                      >
                         Testomonial
                      </Link>
