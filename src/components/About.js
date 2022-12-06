4a280aed426ec8719d0414047bbbecc44a370729
Ramazan Doğan <doganrmzn40@gmail.com>
2022-12-06 15:49:57 +0300

    About and Assets Added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,50 @@
+import Aboutjpg from '../assets/img/about.jpg';
 import React from 'react';
 
 function About() {
-   return <div className=" bg-gray-600 p-4 text-center text-white "></div>;
+   return (
+      <section className=" min-h-screen gap-4">
+         <div className=" text-center">
+            <h2 className=" text-5xl py-2">About Me</h2>
+            <span className=" text-gray-700 text-sm ">My Introduction</span>
+            <img
+               src={Aboutjpg}
+               className=" rounded-tr-xl rounded-bl-xl w-48 my-10 border-2 justify-center self-center mx-auto"
+            />
+            <p className="max-w-xl mx-auto py-2 m-10">
+               Mal oruspu burak Some introduction about me, Some introduction
+               about me, Some introduction about me
+            </p>
+         </div>
+
+         <div className="p-4 flex justify-evenly m-10">
+            <div>
+               <span className="block text-center text-xl font-semibold text-gray-900 ">
+                  01+
+               </span>
+               <span className="block text-center text-l">
+                  Years <br /> Experience
+               </span>
+            </div>
+            <div>
+               <span className="block text-center text-xl font-semibold text-gray-900 ">
+                  17+
+               </span>
+               <span className="block text-center text-l">
+                  Completed <br /> Project
+               </span>
+            </div>
+            <div>
+               <span className="block text-center text-xl font-semibold text-gray-900 ">
+                  02+
+               </span>
+               <span className="block text-center text-l">
+                  Active <br /> Website
+               </span>
+            </div>
+         </div>
+      </section>
+   );
 }
 
 export default About;
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,19 +4,21 @@ import React from 'react';
 
 function Presentation() {
    return (
-      <div>
-         <div className=" mt-10 p-10 text-center ">
-            <h1 className=" text-5xl text-violet-500 hover:text-violet-700hover:">
-               Ramazan Doğan
-            </h1>
-            <h2 className=" text-2xl mt-3">React, Web Developer.</h2>
-            <p className=" max-w-4xl  justify-center text-md py-5 text-gray-800 leading-8 mt-3">
-               Engineer student, trying to be web developer with using Reactjs
-               Engineer student, trying to be web developer with using Reactjs
-               Engineer student, trying to be web developer with using Reactjs
-            </p>
-         </div>
-         {/* <div className="flex justify-center gap-16 ">
+      <>
+         <section className=" min-h-screen gap-4">
+            <div className=" mt-10 py-2 text-center ">
+               <h2 className=" text-5xl py-2 text-violet-500 hover:text-violet-700hover:">
+                  Ramazan Doğan
+               </h2>
+               <h3 className=" text-2xl mt-3">React, Web Developer.</h3>
+               <p className=" max-w-4xl mx-auto  justify-center text-md py-5 text-gray-800 leading-8 mt-3 ">
+                  Engineer student, trying to be web developer with using
+                  Reactjs Engineer student, trying to be web developer with
+                  using Reactjs Engineer student, trying to be web developer
+                  with using Reactjs
+               </p>
+            </div>
+            {/* <div className="flex justify-center gap-16 ">
             <Link to="">
                <button className=" text-3xl w-20 h-20 bg-gray-600 text-white hover:bg-slate-800 hover:bg-gray-900 ">
                   G
@@ -33,13 +35,14 @@ function Presentation() {
                </button>
             </Link>
          </div> */}
-         <div className=" mt-3 text-center">
-            <button className=" px-10 py-5 rounded-tr-2xl rounded-bl-2xl bg-violet-600 text-xl text-white text-center hover:bg-violet-300 hover:text-black">
-               Scroll Down 👇🏻
-            </button>
-         </div>
+            <div className=" mt-3 text-center">
+               <button className=" justify-center px-10 py-5 rounded-tr-2xl rounded-bl-2xl bg-violet-600 text-xl text-white text-center hover:bg-violet-300 hover:text-black">
+                  Scroll Down 👇🏻
+               </button>
+            </div>
+         </section>
          <About />
-      </div>
+      </>
    );
 }
 
