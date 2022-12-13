9dc43380fb0a099812e7bfd587a705313698e6c1
Ramazan Doğan <doganrmzn40@gmail.com>
2022-12-13 20:28:24 +0300

    Grid Column Added
    Little changes.

--- src/components/About.js
+++ src/components/About.js
@@ -4,43 +4,51 @@ import React from 'react';
 function About() {
    return (
       <section className=" border-b-2 border-violet-300 border-t-2 min-h-screen gap-4">
-         <div className=" text-center">
-            <h2 className=" text-5xl my-4 py-2">About Me</h2>
+         <div className=" py-10 text-center">
+            <h2 className=" font-semibold text-5xl my-4 py-2">About Me</h2>
             <span className=" text-gray-700 text-sm ">My Introduction</span>
+         </div>
+         <div className=" grid-cols-2 grid mx-40  ">
             <img
                src={Aboutjpg}
-               className=" rounded-tr-xl rounded-bl-xl w-48 my-10 border-2 justify-center self-center mx-auto"
+               className=" rounded-full w-96 my-10 border-2 justify-center self-center mx-auto"
             />
-            <p className="max-w-xl mx-auto py-2 m-10">
-               Some introduction about me, Some introduction about me, Some
-               introduction about me
-            </p>
-         </div>
-
-         <div className="p-4 flex justify-evenly m-10">
             <div>
-               <span className="block text-center text-xl font-semibold text-gray-900 ">
-                  01+
-               </span>
-               <span className="block text-center text-l">
-                  Years <br /> Experience
-               </span>
-            </div>
-            <div>
-               <span className="block text-center text-xl font-semibold text-gray-900 ">
-                  17+
-               </span>
-               <span className="block text-center text-l">
-                  Completed <br /> Project
-               </span>
-            </div>
-            <div>
-               <span className="block text-center text-xl font-semibold text-gray-900 ">
-                  02+
-               </span>
-               <span className="block text-center text-l">
-                  Active <br /> Website
-               </span>
+               <div className=" grid text-center">
+                  <p className="max-w-xl mx-auto py-2 m-10">
+                     Some introduction about me, Some introduction about me,
+                     Some introduction about me Some introduction about me, Some
+                     introduction about me, Some introduction about me Some
+                     introduction about me, Some introduction about me, Some
+                     introduction about me
+                  </p>
+               </div>
+               <div className="p-4 flex justify-evenly">
+                  <div>
+                     <span className="block text-center text-xl font-semibold text-gray-900 ">
+                        01+
+                     </span>
+                     <span className="block text-center text-l">
+                        Years <br /> Experience
+                     </span>
+                  </div>
+                  <div>
+                     <span className="block text-center text-xl font-semibold text-gray-900 ">
+                        17+
+                     </span>
+                     <span className="block text-center text-l">
+                        Completed <br /> Project
+                     </span>
+                  </div>
+                  <div>
+                     <span className="block text-center text-xl font-semibold text-gray-900 ">
+                        02+
+                     </span>
+                     <span className="block text-center text-l">
+                        Active <br /> Website
+                     </span>
+                  </div>
+               </div>
             </div>
          </div>
       </section>
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -6,73 +6,85 @@ import React from 'react';
 function Presentation() {
    return (
       <>
-         <section className=" min-h-screen gap-4">
-            <div className=" mt-10 py-2 text-center ">
+         <section className=" grid mt-20 mx-40 grid-cols-2 min-h-screen gap-4">
+            <div className="  py-2 text-center ">
+               <h2 className=" text-5xl py-2 text-violet-500 hover:text-violet-700hover:">
+                  Name Here
+               </h2>
+               <h3 className=" text-lg">React, Web Developer.</h3>
+               <p className=" max-w-4xl mx-auto  justify-center text-md py-5 text-gray-800  mt-3 ">
+                  Engineer student, trying to be web developer with using
+                  Reactjs Engineer student, trying to be web developer with
+                  using Reactjs Engineer student, trying to be web developer
+                  using Reactjs Engineer student, trying to be web developer
+                  using Reactjs Engineer student, trying to be web developer
+                  with using Reactjs
+               </p>
+
+               <a href="#about">
+                  <button className="px-10 py-3 rounded-tr-2xl rounded-bl-2xl bg-violet-600 text-xl text-white text-center hover:bg-violet-300 hover:text-black">
+                     Contact Me
+                  </button>
+               </a>
+            </div>
+            <div>
                <img
                   src={Perfil}
                   className="w-60 my-10 justify-center self-center mx-auto"
                />
-               <h2 className=" text-5xl py-2 text-violet-500 hover:text-violet-700hover:">
-                  Name Here
-               </h2>
-               <h3 className=" text-2xl mt-3">React, Web Developer.</h3>
-               <p className=" max-w-4xl mx-auto  justify-center text-md py-5 text-gray-800 leading-8 mt-3 ">
-                  Engineer student, trying to be web developer with using
-                  Reactjs Engineer student, trying to be web developer with
-                  using Reactjs Engineer student, trying to be web developer
-                  with using Reactjs
-               </p>
             </div>
-
-            <div className="text-center">
-               <a href="#about">
-                  <button className=" justify-center px-10 py-5 rounded-tr-2xl rounded-bl-2xl bg-violet-600 text-xl text-white text-center hover:bg-violet-300 hover:text-black">
-                     Scroll Down 👇🏻
-                  </button>
-               </a>
-            </div>
+            <a href="#about">
+               <div className="flex right-0 left-0 ml-auto mr-auto hover:text-cyan-100">
+                  Aşağı Kaydır
+               </div>
+            </a>
          </section>
-         <section
-            id="about"
-            className=" border-b-2 border-violet-300 border-t-2 min-h-screen gap-4"
-         >
-            <div className=" text-center">
-               <h2 className=" text-5xl my-4 py-2">About Me</h2>
+         <section className=" border-b-2 border-violet-300 border-t-2 min-h-screen gap-4">
+            <div className=" py-10 text-center">
+               <h2 className=" font-semibold text-5xl my-4 py-2">About Me</h2>
                <span className=" text-gray-700 text-sm ">My Introduction</span>
+            </div>
+            <div className=" grid-cols-2 grid mx-40  ">
                <img
                   src={Aboutjpg}
-                  className=" rounded-tr-xl rounded-bl-xl w-48 my-10 border-2 justify-center self-center mx-auto"
+                  className="rounded-full w-96 my-10 border-2 justify-center self-center mx-auto"
                />
-               <p className="max-w-xl mx-auto py-2 m-10">
-                  Some introduction about me, Some introduction about me, Some
-                  introduction about me
-               </p>
-            </div>
-
-            <div className="p-4 flex justify-evenly m-10">
                <div>
-                  <span className="block text-center text-xl font-semibold text-gray-900 ">
-                     01+
-                  </span>
-                  <span className="block text-center text-l">
-                     Years <br /> Experience
-                  </span>
-               </div>
-               <div>
-                  <span className="block text-center text-xl font-semibold text-gray-900 ">
-                     17+
-                  </span>
-                  <span className="block text-center text-l">
-                     Completed <br /> Project
-                  </span>
-               </div>
-               <div>
-                  <span className="block text-center text-xl font-semibold text-gray-900 ">
-                     02+
-                  </span>
-                  <span className="block text-center text-l">
-                     Active <br /> Website
-                  </span>
+                  <div className=" grid text-center">
+                     <p className="max-w-xl mx-auto py-2 m-10">
+                        Some introduction about me, Some introduction about me,
+                        Some introduction about me Some introduction about me,
+                        Some introduction about me, Some introduction about me
+                        Some introduction about me, Some introduction about me,
+                        Some introduction about me
+                     </p>
+                  </div>
+                  <div className="p-4 flex justify-evenly">
+                     <div>
+                        <span className="block text-center text-xl font-semibold text-gray-900 ">
+                           01+
+                        </span>
+                        <span className="block text-center text-l">
+                           Years <br /> Experience
+                        </span>
+                     </div>
+                     <div>
+                        <span className="block text-center text-xl font-semibold text-gray-900 ">
+                           17+
+                        </span>
+                        <span className="block text-center text-l">
+                           Completed <br /> Project
+                        </span>
+                     </div>
+                     <div>
+                        <span className="block text-center text-xl font-semibold text-gray-900 ">
+                           02+
+                        </span>
+                        <span className="block text-center text-l">
+                           Active <br /> Website
+                        </span>
+                     </div>
+                  </div>
                </div>
             </div>
          </section>
