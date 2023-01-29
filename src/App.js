b79bc544a42fcd057db3be60a0cd97256180d8c7
Ramazan Doğan <doganrmzn40@gmail.com>
2023-01-29 18:40:38 +0300

    skills section is over

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import About from './components/Body/About';
 import Presentation from './components/Body/Presentation';
+import Skills from './components/Body/Skills/Skills';
 
 function App() {
    return (
       <div>
          <Presentation />
          <About />
+         <Skills />
       </div>
    );
 }
---
+++ src/components/Body/Skills/Skills.jsx
@@ -0,0 +1,212 @@
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
+import { FaQuoteLeft, FaQuoteRight, FaReact } from 'react-icons/fa';
+import React, { useState } from 'react';
+
+import { GiSkills } from 'react-icons/gi';
+import { RxBorderDashed } from 'react-icons/rx';
+import styles from './skills.module.css';
+
+function Skills() {
+   const [show, setShow] = useState(true);
+   const [show1, setShow1] = useState(true);
+
+   return (
+      <section className={styles.section}>
+         <div id="skills">
+            <h2 className={styles.title}>
+               Skills <GiSkills />
+            </h2>
+            <p className={styles.skillsP}>My technical level</p>
+         </div>
+         <div className={styles.container}>
+            <div className={styles.secondContainer}>
+               <div
+                  onClick={() => setShow(!show)}
+                  className={styles.dropdownContainer}
+               >
+                  <div className={styles.skillContainer}>
+                     <FaReact className={styles.skillIcon} />
+                     <div className={styles.skillSubContainer}>
+                        <h3 className={styles.skillTitle}>Frontend Skill</h3>
+
+                        <p className={styles.skillP}>More then 1 years</p>
+                     </div>
+                  </div>
+                  {show === true ? (
+                     <AiOutlineArrowUp className={styles.upDownIcon} />
+                  ) : (
+                     <AiOutlineArrowDown className={styles.upDownIcon} />
+                  )}
+               </div>
+               {show === true ? (
+                  <div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>HTML5</div>
+                           <div className={styles.skillItemPercentage}>
+                              100%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage100}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>CSS3</div>
+                           <div className={styles.skillItemPercentage}>70%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage70}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>
+                              Javascript
+                           </div>
+                           <div className={styles.skillItemPercentage}>60%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage60}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>React</div>
+                           <div className={styles.skillItemPercentage}>
+                              100%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage100}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>Tailwind</div>
+                           <div className={styles.skillItemPercentage}>80%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage80}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>Bootstrap</div>
+                           <div className={styles.skillItemPercentage70}>
+                              70%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage70}></div>
+                        </div>
+                     </div>
+                  </div>
+               ) : null}
+            </div>
+            <div className={styles.secondContainer}>
+               <div
+                  onClick={() => setShow1(!show1)}
+                  className={styles.dropdownContainer}
+               >
+                  <div className={styles.skillContainer}>
+                     <FaReact className={styles.skillIcon} />
+                     <div className={styles.skillSubContainer}>
+                        <h3 className={styles.skillTitle}>Frontend Skill</h3>
+
+                        <p className={styles.skillP}>More then 1 years</p>
+                     </div>
+                  </div>
+                  {show1 === true ? (
+                     <AiOutlineArrowUp className={styles.upDownIcon} />
+                  ) : (
+                     <AiOutlineArrowDown className={styles.upDownIcon} />
+                  )}
+               </div>
+               {show1 === true ? (
+                  <div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>HTML5</div>
+                           <div className={styles.skillItemPercentage}>
+                              100%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage100}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>CSS3</div>
+                           <div className={styles.skillItemPercentage}>70%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage70}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>
+                              Javascript
+                           </div>
+                           <div className={styles.skillItemPercentage}>60%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage60}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>React</div>
+                           <div className={styles.skillItemPercentage}>
+                              100%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage100}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>Tailwind</div>
+                           <div className={styles.skillItemPercentage}>80%</div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage80}></div>
+                        </div>
+                     </div>
+                     <div className={styles.skillsListContainer}>
+                        <div className={styles.listContainerItems}>
+                           <div className={styles.skillItemName}>Bootstrap</div>
+                           <div className={styles.skillItemPercentage70}>
+                              70%
+                           </div>
+                        </div>
+                        <div className={styles.percentageBar}>
+                           <div className={styles.percentage70}></div>
+                        </div>
+                     </div>
+                  </div>
+               ) : null}
+            </div>
+         </div>
+         <div className={styles.secondSection}>
+            <div className={styles.topQuoteIcon}>
+               <FaQuoteLeft />
+            </div>
+            <div className={styles.quote}>
+               İyi eleman seç, iyi para ver. <br /> Çünkü kötü eleman, sana
+               pahalıya mal olur. <br />
+               <div className={styles.quoteName}>Vehbi Koç</div>
+            </div>
+            <div className={styles.quoteBorder}>
+               <RxBorderDashed />
+            </div>
+         </div>
+      </section>
+   );
+}
+
+export default Skills;
